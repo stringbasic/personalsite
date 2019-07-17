deddefaab424fdfde6055e1480c26c8053c5d41f
Nelson Ruivo <nruivo@thefork.pt>
2019-07-17 14:59:24 +0100

    Configured default cypress test url.

--- cypress/integration/homepage_spec.js
+++ cypress/integration/homepage_spec.js
@@ -1,6 +1,6 @@
 describe('Homepage check', function() {
   it('Does open', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('/')
     cy.get('p:first').should('contain', 'StringBasic website is currently under construction.')
   })
 })

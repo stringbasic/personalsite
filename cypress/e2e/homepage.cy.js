9d8e896960ce86e5e2f50af6a248cc2fe1321708
Nelson Ruivo <nelsonruivo@gmail.com>
2023-04-24 12:31:19 +0100

    Add build time footer line.

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,5 +2,15 @@ describe('Homepage check', function() {
   it('Does open', function() {
     cy.visit('/')
     cy.get('p:first').should('contain', 'StringBasic website is currently under construction.')
-  })
+  });
+  it("Check build time to be less or equal than now", () => {
+    cy.visit('/')
+    cy.get('b:first').invoke('text').then(actualDateText => {
+      const dayjs = require('dayjs');
+      const todaysDate = new Date();
+      const actualDate = dayjs(actualDateText, 'YYYY-MM-DD HH:mm:ss').toDate();
+
+      expect(actualDate).to.be.lte(todaysDate);
+    });
+  });
 })
